be80622a60984c45dff3cda2fb1a0cc53e00c925
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2024-06-11 18:39:50 +0530

    Added coded for refresh token functionality

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -29,14 +29,14 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 
 }
 
-func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
 	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
-	return token, err
+	return token, payload, err
 
 }
 
